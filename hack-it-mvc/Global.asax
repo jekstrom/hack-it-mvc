a874ce0d56bfad216df066a995282070e262b1cb
Don Waldo <don.waldo@arinet.com>
2016-04-22 22:29:00 -0500

    Adds web app

---
+++ hack-it-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hack_it_mvc.MvcApplication" Language="C#" %>
---
+++ hack-it-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hack_it_mvc.Startup))]
+namespace hack_it_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
